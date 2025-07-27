eae26e9177f9b5249419a863a2d00ad3041b81f6
Ngoc Thien <ngocthien.dev23@gmail.com>
2025-07-27 09:56:16 +0700

    fix github data

--- src/types/api/github.ts
+++ src/types/api/github.ts
@@ -1,3 +1,4 @@
+// src/types/api/github.ts
 export interface GitHubRepo {
   id: number;
   name: string;
@@ -15,6 +16,23 @@ export interface GitHubRepo {
   updated_at: string;
   pushed_at: string;
   default_branch: string;
+  fork: boolean;
+  private: boolean;
+  archived: boolean;
+  disabled: boolean;
+  size: number;
+  clone_url: string;
+  ssh_url: string;
+  git_url: string;
+  svn_url: string;
+  mirror_url: string | null;
+  license: {
+    key: string;
+    name: string;
+    spdx_id: string;
+    url: string | null;
+    node_id: string;
+  } | null;
 }
 
 export interface GitHubUser {
